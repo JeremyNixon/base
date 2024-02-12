81736591d9408b1d4066c35c6621f8c1a2ebfd95
Jeremy Nixon <jnixon2@gmail.com>
2024-02-12 19:57:56 +0000

    update database setup

--- boilerplate/jupyter_notebook_config.py
+++ boilerplate/jupyter_notebook_config.py
@@ -5,4 +5,4 @@ c.NotebookApp.allow_origin = '*'
 c.NotebookApp.ip = '0.0.0.0'
 
 c.NotebookApp.token = ''
-c.NotebookApp.password = 'template'
+# Password is: template
--- deploy.sh
+++ deploy.sh
@@ -12,6 +12,19 @@ pip install notebook
 pip3 install jupyterlab
 jupyter notebook --generate-config
 sudo cp boilerplate/jupyter_notebook_config.py ~/.jupyter/jupyter_notebook_config.py
+sudo cp boilerplate/jupyter_server_config.json ~/.jupyter/jupyter_server_config.json
+sudo apt update
+sudo apt install postgresql postgresql-contrib
+sudo -i -u postgres
+createuser omni
+createdb truthriver
+psql -c "ALTER USER omni WITH PASSWORD 'planttheknowledgetree';"
+psql -c "GRANT ALL PRIVILEGES ON DATABASE truthriver TO omni;"
+exit
+sudo cp boilerplate/postgresql.conf /etc/postgresql/13/main/postgresql.conf
+sudo cp boilerplate/pg_hba.conf /etc/postgresql/13/main/pg_hba.conf
+sudo systemctl reload postgresql@13-main
+
 source ~/.bash_profile
 tmux new-session -s lab -d 'jupyter lab \
   --port=8080 \
