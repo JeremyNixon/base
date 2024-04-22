e6d39797cb14357b136e2196dbccfa70f4fb547c
Jeremy Nixon <jnixon2@gmail.com>
2024-04-22 13:07:48 -0700

    update with  update -y

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
+sudo apt update -y
+sudo apt upgrade -y  # Automatically upgrades packages without user interaction
 sudo apt install -y nginx
 sudo cp boilerplate/flask_app /etc/nginx/sites-enabled/flask_app
 sudo cp boilerplate/.bash_profile ~/.bash_profile
@@ -13,7 +15,6 @@ pip3 install jupyterlab
 jupyter notebook --generate-config
 sudo cp boilerplate/jupyter_notebook_config.py ~/.jupyter/jupyter_notebook_config.py
 sudo cp boilerplate/jupyter_server_config.json ~/.jupyter/jupyter_server_config.json
-sudo apt update -y
 sudo apt install -y postgresql postgresql-contrib
 sudo -i -u postgres
 createuser omni
@@ -31,4 +32,3 @@ tmux new-session -s lab -d 'jupyter lab \
   --NotebookApp.port_retries=0 \
   --allow-root'
 tmux new-session -s prod -d "gunicorn3 --workers=3 --bind 0.0.0.0:8000 --timeout 60000 routes:app"
-
