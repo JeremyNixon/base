620ada6239360bcc4e820da3f53e01efdbf3b2b6
Jeremy Nixon <jnixon2@gmail.com>
2024-04-22 13:23:30 -0700

    update readme and deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,21 @@
-sudo apt install nginx
+sudo apt update -y
+sudo apt upgrade -y  # Automatically upgrades packages without user interaction
+sudo apt install -y nginx
 sudo cp boilerplate/flask_app /etc/nginx/sites-enabled/flask_app
 sudo cp boilerplate/.bash_profile ~/.bash_profile
 sudo unlink /etc/nginx/sites-enabled/default
 sudo nginx -s reload
-sudo apt install python3
-sudo apt install python3-pip
+sudo apt install -y python3
+sudo apt install -y python3-pip
 pip3 install -r requirements.txt
-sudo apt install gunicorn3
-sudo apt install tmux
+sudo apt install -y gunicorn3
+sudo apt install -y tmux
 pip install notebook
 pip3 install jupyterlab
 jupyter notebook --generate-config
 sudo cp boilerplate/jupyter_notebook_config.py ~/.jupyter/jupyter_notebook_config.py
 sudo cp boilerplate/jupyter_server_config.json ~/.jupyter/jupyter_server_config.json
-sudo apt update
-sudo apt install postgresql postgresql-contrib
+sudo apt install -y postgresql postgresql-contrib
 sudo -i -u postgres
 createuser omni
 createdb truthriver
@@ -30,5 +31,4 @@ tmux new-session -s lab -d 'jupyter lab \
   --port=8080 \
   --NotebookApp.port_retries=0 \
   --allow-root'
-tmux new-session -s prod -d "gunicorn3 --workers=3 --bind 0.0.0.0:8000 --timeout 60000 routes:app"'
-
+tmux new-session -s prod -d "gunicorn3 --workers=3 --bind 0.0.0.0:8000 --timeout 60000 routes:app"
